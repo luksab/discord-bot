0fdebd1c46022b69460487fe802584df47e94355
Lukas Sabatschus <luksablp@gmail.com>
2020-09-12 23:23:29 +0200

    add invite command

---
+++ commands/link.js
@@ -0,0 +1,11 @@
+const {
+	invite,
+} = require('./config.json');
+
+module.exports = {
+	name: 'link',
+	description: 'Get invite link.',
+	execute(message) {
+		message.channel.send(invite);
+	},
+};
