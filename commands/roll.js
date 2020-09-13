05ab9b2ce85b746259c0ba24514ad70013d1a618
Lukas Sabatschus <luksablp@gmail.com>
2020-09-13 17:10:30 +0200

    add dice rolling

---
+++ commands/roll.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: 'roll',
+	description: 'roll dice in dnd notation (eg. 2d20)',
+	execute(message) {
+		const args = message.content.split(" ");
+		const dice = args[1].split("d");
+		if (dice[0] > 1000) return message.channel.send("Thats too many dice - I don't want to overload my server.")
+		const throws = Array.from({ length: dice[0] }, () => (Math.random() * dice[1] + 1.) | 0);
+
+		const msg = throws.join(",") + (throws.length > 1 ? (" = " + throws.reduce((a, b) => a + b)) : "")
+		if(msg.length > 2000)return message.channel.send("Thats too many dice - discord messages can only be 2000 characters in length.")
+		message.channel.send(msg);
+	},
+};
