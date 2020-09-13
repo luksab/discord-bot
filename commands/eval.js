f5191e46e9afa6f84d9e789f3e4899c0c889a1cf
Lukas Sabatschus <luksablp@gmail.com>
2020-09-13 20:57:45 +0200

    tweak eval string

--- commands/eval.js
+++ commands/eval.js
@@ -7,7 +7,7 @@ module.exports = {
 	execute(message, client) {
 		if (message.author.id === "393450329724682240") {
 			const msg = eval(message.content.substring("$js ".length));
-			message.channel.send("error: \n```" + msg + "```", { split: { "maxLength": 2000 } });
+			message.channel.send("```" + msg + "```", { split: { "maxLength": 2000 } });
 		}
 		else return message.channel.send("You don't have permission over me!");
 	},
