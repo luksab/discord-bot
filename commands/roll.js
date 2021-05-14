1a2037a408b4966434fbe6d5a3e1792404ee34a7
Lukas Sabatschus <luksablp@gmail.com>
2021-05-14 23:52:19 +0200

    fix 0 dice

--- commands/roll.js
+++ commands/roll.js
@@ -4,6 +4,7 @@ module.exports = {
 	execute(message) {
 		const args = message.content.split(" ");
 		const dice = args[1].split("d");
+		if (dice[0] < 1) return message.channel.send("I can't roll < 1 dice, dummy.")
 		if (dice[0] > 1000) return message.channel.send("Thats too many dice - I don't want to overload my server.")
 		const throws = Array.from({ length: dice[0] }, () => (Math.random() * dice[1] + 1.) | 0);
 
